fba89e5c218e87762c4bc37d509cc4b1602f103e
ymd65536 <ymd65536@gmail.com>
2024-02-23 01:09:41 +0900

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Momento.Sdk;
+using Momento.Sdk.Auth;
+using Momento.Sdk.Config;
+using Momento.Sdk.Responses;
+
+ICredentialProvider authProvider = new EnvMomentoTokenProvider("MOMENTO_AUTH_TOKEN");
+const string CACHE_NAME = "BlazorApp";
+TimeSpan DEFAULT_TTL = TimeSpan.FromSeconds(60);
+
+ICacheClient client = new CacheClient(Configurations.Laptop.V1(), authProvider, DEFAULT_TTL);
+
+string KEY = "BlazorCounter";
+string VALUE = "10";
+int new_value = int.Parse(VALUE) + 1;
+VALUE = new_value.ToString();
+
+Console.WriteLine($"Setting key: {KEY} with value: {VALUE}");
+
+var setResponse = await client.SetAsync(CACHE_NAME, KEY, VALUE);
+if (setResponse is CacheSetResponse.Error setError)
+{
+    Console.WriteLine($"Error setting value: {setError.Message}. Exiting.");
+    Environment.Exit(1);
+}
+
+Console.WriteLine($"Get value for key: {KEY}");
+CacheGetResponse getResponse = await client.GetAsync(CACHE_NAME, KEY);
+if (getResponse is CacheGetResponse.Hit hitResponse)
+{
+    Console.WriteLine($"Looked up value: {hitResponse.ValueString}, Stored value: {VALUE}");
+}
+else if (getResponse is CacheGetResponse.Error getError)
+{
+    Console.WriteLine($"Error getting value: {getError.Message}");
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # MomentoCacheInDotNET
+
 .NETでMomneto Cacheを実行する
+
+## setup
+
+C#のパッケージを追加します。
+
+```bash
+dotnet add package Momento.Sdk
+```
+
+APIキーを環境変数に設定します。
+
+```bash
+export MOMENTO_AUTH_TOKEN="token"
+```
+
+## 参考
+
+[Getting Started with Momento Cache in .NET](https://docs.momentohq.com/sdks/dotnet/cache.html)
